e56ba0e2c888e3ffcb8a9091ff9b24835baaf312
Aly Youssef <alyhassan2254@gmail.com>
2025-12-09 10:26:50 +0200

    added helper functions

--- main.cpp
+++ main.cpp
@@ -7,6 +7,51 @@
 #include <algorithm>
 using namespace std;
 
+int getRegisterNumber(const string& reg) {
+    if (reg.size() < 2 || reg[0] != 'r')
+        return -1; // does not have an r
+
+    return stoi(reg.substr(1));  // convert everything after 'r'
+}
+
+struct MemAddress {
+    int offset;
+    int baseReg;
+};
+
+// input: "4(r2)"
+MemAddress decodeMemAddress(const string& op) {
+    MemAddress m{0, 0};
+
+    // get '(' and ')'
+    size_t l = op.find('(');
+    size_t r = op.find(')');
+
+    // offset
+    m.offset = stoi(op.substr(0, l));
+
+    // base register name: r2
+    string reg = op.substr(l + 1, r - l - 1);
+    m.baseReg = getRegisterNumber(reg);
+
+    return m;
+}
+
+int encodeSigned5(int value) {
+    // make sure value fits in 5 bits
+    if (value < -16 || value > 15) {
+        cerr << "Offset out of 5-bit signed range\n";
+        return 0;
+    }
+
+    // make it a 5-bit two's complement
+    if (value < 0)
+        value = (1 << 5) + value;  // add 32 to wrap
+
+    return value & 0x1F;  // keep only 5 bits
+}
+
+
 string trim(string s) {
     // Remove leading spaces
     while (!s.empty() && isspace(s.front()))
